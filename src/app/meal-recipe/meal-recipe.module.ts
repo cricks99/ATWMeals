5346d8b6f9d0022c42278e326dd4c9487a607ba2
kimtasticc <107227953+kimtasticc@users.noreply.github.com>
2023-03-24 16:55:45 -0400

    finished country and ingredient functionality

--- src/app/meal-recipe/meal-recipe.module.ts
+++ src/app/meal-recipe/meal-recipe.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
 import { RecipeDetailsComponent } from './recipe-details/recipe-details.component';
 import { RatingsComponent } from './ratings/ratings.component';
+import { FormsModule } from '@angular/forms';
 import { UserProfileModule } from '../user-profile/user-profile.module';
 
 
@@ -14,7 +15,8 @@ import { UserProfileModule } from '../user-profile/user-profile.module';
     RatingsComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   exports: [
     RecipeListComponent,
