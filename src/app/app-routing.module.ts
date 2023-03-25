a2751e5f18c8c6b1095385289758b55990881b75
craig <craig@ten987.com>
2023-03-25 09:52:47 -0400

    added routing, user login, and added user object availability to recipe list component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './core/home/home.component';
+import { PageNotFoundComponent } from './core/page-not-found/page-not-found.component';
+import { UserLoginComponent } from './user-login/user-login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home-component', component: HomeComponent },
+  { path: 'user-login', component: UserLoginComponent },
+  { path: '', redirectTo: '/home-component', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/user-login/user-login.component.ts
+++ src/app/user-login/user-login.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IUser } from '../user-profile/interfaces/user';
 import { UserRepositoryService } from '../user-profile/user-repository.service';
+import { NgForm } from '@angular/forms';
+import { LocalService } from '../local.service';
 
 @Component({
   selector: 'app-user-login',
@@ -8,27 +10,57 @@ import { UserRepositoryService } from '../user-profile/user-repository.service';
   styleUrls: ['./user-login.component.css']
 })
 export class UserLoginComponent {
-  user: IUser | undefined;
-  
-  userTest:any = {name: "TestNewUser", password: "Password7"};
+  user: IUser = {id: 0, name: "", password: "", favorites: [], passports: [], mealRatings: []}
+  //userTest:any = {name: "Bob", password: "Bob123"};
+  //userId: number = 0;
 
-  constructor(private repo:UserRepositoryService) {}
+  constructor(private repo: UserRepositoryService, private localStore: LocalService) { }
 
   ngOnInit(): void {
     //this.addUser(this.userTest);  //this will only get created first time.  If exists, will return 0 for id
-    this.getUser(this.userTest);
+    //this.getUser(this.userTest);
+    
+    this.getUserObject();
   }
 
-  getUser(userLogin: any)
-  {
-    this.repo.getUserByPassword(userLogin).subscribe (
-      (response) => {this.user = response;});
+  getUserObject() {
+    let savedUserId = this.localStore.getData("userId");
+
+    if (savedUserId && +savedUserId > 0) {
+      this.repo.getUserById(savedUserId).subscribe (
+        (response) => {this.user = response;});
+    }
   }
 
   addUser(userLogin: any)
   {
     this.repo.addUser(userLogin).subscribe (
       (response) => {this.user = response;});
+
   }
 
+  loginUser(form: NgForm) {
+    let userName = form.form.value.userName;
+    let password = form.form.value.password;
+    
+    this.getUser({name: userName, password: password});
+  }
+
+   //API returns id = -1 if wrong username/password
+   getUser(userLogin: any)
+   {
+     this.repo.getUserByPassword(userLogin).subscribe (
+       (response) => {
+         this.user = response;
+         
+         if (this.user.id > 0)
+           this.localStore.saveData("userId", this.user.id.toString());   
+       });
+   }
+ 
+  logoutUser()
+  {
+    this.user.id = 0;
+    this.localStore.removeData("userId");
+  }
 }
