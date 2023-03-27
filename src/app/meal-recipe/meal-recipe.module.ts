a1c4d7bacbb8b5f8b6d84fb0be6fbd47e48182f1
craig <craig@ten987.com>
2023-03-27 03:41:05 -0400

    partial work on ratings component

--- src/app/meal-recipe/meal-recipe.module.ts
+++ src/app/meal-recipe/meal-recipe.module.ts
@@ -4,7 +4,6 @@ import { RecipeListComponent } from './recipe-list/recipe-list.component';
 import { RecipeDetailsComponent } from './recipe-details/recipe-details.component';
 import { RatingsComponent } from './ratings/ratings.component';
 import { FormsModule } from '@angular/forms';
-import { UserProfileModule } from '../user-profile/user-profile.module';
 import { HomeComponent } from '../core/home/home.component';
 import {MatIconModule} from '@angular/material/icon';
 import { AppRoutingModule } from '../app-routing.module';
@@ -29,7 +28,6 @@ const materialModules = [
   exports: [
     RecipeListComponent,
     RecipeDetailsComponent,
-    RatingsComponent,
     materialModules
   ]
 })
